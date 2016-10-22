f85b3d85ff05967fb529f246695bfe4bf2f555f3
kubale69 <burnnnnnn@gmail.com>
2016-10-22 15:23:08 +0200

    pozdro

---
+++ Generator/src/generator/Generator.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generator;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+/**
+ *
+ * @author MSI
+ */
+public class Generator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        List<String> listaImionM = new ArrayList<String>();
+        List<String> listaImionF = new ArrayList<String>();
+        List<String> listaNazwisk = new ArrayList<String>();
+        List<Lekarz> lekarze = new ArrayList<Lekarz>();
+        String textLine;
+        int sex;
+        Random rand = new Random();
+        BufferedReader br = new BufferedReader (new FileReader("imionam.txt"));
+        while ((textLine = br.readLine()) != null) {
+            listaImionM.add(textLine);
+        }
+        BufferedReader br2 = new BufferedReader (new FileReader("nazwiska.txt"));
+        while ((textLine = br2.readLine()) != null) {
+            listaNazwisk.add(textLine);
+        }
+         BufferedReader br3 = new BufferedReader (new FileReader("imionaf.txt"));
+        while ((textLine = br3.readLine()) != null) {
+            listaImionF .add(textLine);
+        }
+        
+        for (int i = 0; i<100;i++){
+        lekarze.add(new Lekarz());
+        sex = rand.nextInt(2);
+        if (sex == 0)
+        lekarze.get(i).setImie(listaImionM); 
+        else
+        lekarze.get(i).setImie(listaImionF); 
+        lekarze.get(i).setNazwisko(listaNazwisk, sex);
+        lekarze.get(i).setId(i+1);
+        }
+        
+  
+        for (Lekarz l : lekarze) l.wypisz();
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ Generator/src/generator/Lekarz.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+/**
+ *
+ * @author MSI
+ */
+public class Lekarz {
+    private Integer id;
+    private String specjalizacja;
+    private String imie;
+    private String nazwisko;
+    private Date dataZatrudnienia;
+    private Date dataZwolnienia;
+    
+    public void setId(int id) {
+    this.id = id;
+}
+    public void setImie(List<String> imiona) {
+     Random rand = new Random();
+     int los = rand.nextInt(imiona.size());
+     this.imie = imiona.get(los);  
+    }
+    
+     public void setNazwisko(List<String> nazwiska, int sex) {
+     Random rand = new Random();
+     int los = rand.nextInt(nazwiska.size());
+     String randNazwisko = nazwiska.get(los);
+     String sub = randNazwisko.substring(randNazwisko.length()-3);
+     if (sex == 0 || !sub.equals("ski") && !sub.equals("cki") && !sub.endsWith("zki"))
+     this.nazwisko = nazwiska.get(los);  
+     else
+     this.nazwisko = randNazwisko.substring(0,randNazwisko.length()- 1) + "a";
+    }
+    
+    
+    public void wypisz(){
+    System.out.println(this.id + " " + this.imie + " " + this.nazwisko); 
+    }
+}
+    
